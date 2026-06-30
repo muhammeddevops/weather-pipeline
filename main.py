0e5908b2883fc138e650b61c94a2411120b86c3f
Muhammed <muhammednabeel@live.co.uk>
2026-06-30 19:06:01 +0100

    Add Azure Blob Storage integration for raw weather data

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
 import psycopg2
+import json
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
+
 
 url = "https://api.open-meteo.com/v1/forecast"
 
-user_input = input('Enter a city:')
+user_input = input('Enter a city: ')
 
 geo_url = "https://geocoding-api.open-meteo.com/v1/search"
 
@@ -11,13 +19,13 @@ geo_response = requests.get(geo_url, params={"name": user_input, "count": 1})
 
 geo_data = geo_response.json()
 
-
-city = geo_data['results'][0]['name']
-country = geo_data['results'][0]['country']
-
 if not geo_data.get("results"):
     print("Invalid city. No data found.")
     exit()
+
+
+city = geo_data['results'][0]['name']
+country = geo_data['results'][0]['country']
 
 latitude = geo_data["results"][0]["latitude"]
 longitude = geo_data["results"][0]["longitude"]
@@ -34,6 +42,23 @@ data = response.json()
 
 temperature = data['current_weather']['temperature']
 windspeed = data['current_weather']['windspeed']
+
+filename = 'weather' + datetime.now().strftime("%Y-%m-%d-%H-%M.json")
+
+connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+blob_service = BlobServiceClient.from_connection_string(connection_string)
+
+container = blob_service.get_container_client('weathercontainer')
+
+blob = container.get_blob_client(filename)
+
+
+with open(filename, "w") as file:
+    json.dump(data, file, indent=4)
+
+with open(filename, 'rb') as file:
+    blob.upload_blob(file, overwrite=True)
 
 print(f"In {city}, {country}")
 print('The temperature is currently ', temperature)
